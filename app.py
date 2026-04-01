7e48498252da898d431e08bc54ad750dda174367
eriksokolowski1-cell <eriksokolowski1@gmail.com>
2026-04-01 19:14:57 -0400

    Refactor SanctuaryLatticeV2 and MemoryVault classes

--- app.py
+++ app.py
@@ -219,9 +219,9 @@ user_msg = st.text_input("Send a message to the Sanctuary:")
 
 if user_msg:
     # This is the "+= 1" logic for the interactions
-    lattice.namiko_context["interactions"] += 1
+  st.session_state.lattice.namiko_context["interactions"] += 1
 
-    # Namiko's Response
+   # Namiko's Response
     st.info(f"Namiko [1.605 GHz]: Transmission received, Architect. Current Interaction Level: {lattice.namiko_context['interactions']}")
 
 
